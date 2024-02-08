6528394ef181d3da37d1c5096b0eb411017e1c12
Luis Olavo Dantas <epistemologia@users.noreply.github.com>
2024-02-08 10:03:56 -0300

    Add files via upload

--- csv_para_html.py
+++ csv_para_html.py
@@ -1,17 +1,28 @@
 import glob
 import csv
-# from analisa_arquivo_csv import analisa
-# from filtros_e_excecoes import trata_excecoes
-
+import chardet
+import sys
 
 # Use glob.glob to find CSV files in the current directory
+
+
 def identifica_arquivos_csv() -> list:
     lista_arquivos_csv = glob.glob("*.csv")
     # lista_arquivos_csv.remove("excecoes.csv")
     return lista_arquivos_csv
 
 
-def identify_delimiter(filename: str):
+def detecta_codificacao_csv(nome_arquivo_csv: str):
+
+    with open(nome_arquivo_csv, 'rb') as f:
+        result = chardet.detect(f.read())
+        print(f"O arquivo CSV encontrado tem codificação: {
+              result['encoding']}")
+    codificacao = result['encoding']
+    return codificacao
+
+
+def identify_delimiter(filename: str, codificacao_entrada: str):
     """
     Identifies the delimiter used in a CSV file by counting commas and semicolons in the first five records.
 
@@ -23,22 +34,26 @@ def identify_delimiter(filename: str):
     """
     comma_count = 0
     semicolon_count = 0
+    tab_count = 0
 
-    with open(filename, 'r') as csvfile:
+    with open(filename, 'r', encoding=codificacao_entrada) as csvfile:
         for _ in range(5):
             try:
                 row = csvfile.readline()
                 comma_count += row.count(',')
                 semicolon_count += row.count(';')
+                tab_count += row.count('\n')
             except StopIteration:
                 # Fewer than 5 records, ignore
                 break
-    if comma_count > semicolon_count:
-        return ','
-    elif semicolon_count > comma_count:
-        return ';'
-    else:
-        return None
+    separador = None
+    if (comma_count > tab_count) and (comma_count > semicolon_count):
+        separador = ","
+    if (semicolon_count > comma_count) and (semicolon_count > tab_count):
+        separador = ";"
+    if (tab_count > semicolon_count) and (tab_count > comma_count):
+        separador = "\n"
+    return separador
 
 # Use glob.glob to find "antes.html" files in the current directory
 
@@ -63,24 +78,24 @@ def encontra_css_css() -> bool:
 
 
 def copia_antes_html(nome_de_arquivo: str):
-    with open(nome_de_arquivo, "a", encoding="utf-8") as saida:
-        with open("antes.html", "r", encoding="utf-8") as entrada:
+    with open(nome_de_arquivo, "a", encoding="utf-8-sig") as saida:
+        with open("antes.html", "r", encoding="utf-8-sig") as entrada:
             for conteudo in entrada:
                 saida.write(conteudo)
     return
 
 
 def copia_depois_html(nome_de_arquivo: str):
-    with open(nome_de_arquivo, "a", encoding="utf-8") as saida:
-        with open("depois.html", "r", encoding="utf-8") as entrada:
+    with open(nome_de_arquivo, "a", encoding="utf-8-sig") as saida:
+        with open("depois.html", "r", encoding="utf-8-sig") as entrada:
             for conteudo in entrada:
                 saida.write(conteudo)
     return
 
 
 def copia_css_css(nome_de_arquivo: str):
-    with open(nome_de_arquivo, "a", encoding="utf-8") as saida:
-        with open("css.css", "r", encoding="utf-8") as entrada:
+    with open(nome_de_arquivo, "a", encoding="utf-8-sig") as saida:
+        with open("css.css", "r", encoding="utf-8-sig") as entrada:
             for conteudo in entrada:
                 saida.write(conteudo)
     return
@@ -89,14 +104,14 @@ def copia_css_css(nome_de_arquivo: str):
 def escolhe_arquivo_entrada(x: list) -> str:
     if len(x) == 1:
         return x[0]
-    return ("A determinar")
+    return ("???")
 
 
-def le_databook(nome_de_arquivo: str, delimitador: str) -> tuple:
+def le_databook(nome_de_arquivo: str, delimitador: str, codificacao_entrada: str) -> tuple:
     lista = []
     cabecalhos = []
     contador = -1
-    with open(nome_de_arquivo, 'r', encoding='cp1252') as f:
+    with open(nome_de_arquivo, 'r', encoding=codificacao_entrada) as f:
         reader = csv.reader(f, delimiter=delimitador)
         for registro in reader:
             contador += 1
@@ -127,7 +142,7 @@ def fecha_tabela() -> str:
 def fecha_html(arquivo_saida: str):
     if encontra_depois_html():
         copia_depois_html(arquivo_saida)
-    with open(arquivo_saida, "a", encoding='utf-8') as h:
+    with open(arquivo_saida, "a", encoding='utf-8-sig') as h:
         h.write("</body>\n</html>\n")
     return
 
@@ -145,7 +160,7 @@ def cria_arq_saida(nome_saida: str, cabecalho: list, registros: list):
     if encontra_antes_html():
         copia_antes_html(nome_saida)
     contador = 0
-    with open(nome_saida, 'a', encoding='utf-8') as g:
+    with open(nome_saida, 'a', encoding='utf-8-sig') as g:
         g.write(abre_tabela())
         g.write(cabecalho_tabela(cabecalho))
         for registro in registros:
@@ -157,7 +172,7 @@ def cria_arq_saida(nome_saida: str, cabecalho: list, registros: list):
 
 
 def inicia_html(nome_arquivo_saida: str):
-    with open(nome_arquivo_saida, 'w', encoding='utf-8') as g:
+    with open(nome_arquivo_saida, 'w', encoding='utf-8-sig') as g:
         g.write("""<!doctype html>
 <html lang="pt-br">
 <head>
@@ -217,7 +232,7 @@ tbody td.cnt::before {
 def encerra_css(arquivo_saida: str):
     if encontra_css_css():
         copia_css_css(arquivo_saida)
-    with open(arquivo_saida, 'a', encoding='utf-8') as g:
+    with open(arquivo_saida, 'a', encoding='utf-8-sig') as g:
         g.write("""</style>
 </head>
 <body>
@@ -229,19 +244,33 @@ def main() -> list:
     # lista de nomes de arquivo (com extensão ".csv")
     csv_arquivos = identifica_arquivos_csv()
     arq_ent = escolhe_arquivo_entrada(csv_arquivos)
+    if arq_ent == "???":
+        print("Não há um arquivo CSV único neste diretório.  Tente de novo, por favor, após garantir que exista exatamente um.")
+        x = input("Tecle <enter> para encerrar.")
+        sys.exit(0)
+    codificacao_entrada = detecta_codificacao_csv(arq_ent)
     # Supomos que seja vírgula ou ponto-e-vírgula
-    delimitador = identify_delimiter(arq_ent)
+    delimitador = identify_delimiter(arq_ent, codificacao_entrada)
+    delimitador_traduzido = "* desconhecido *"
+    if delimitador == '\n':
+        delimitador_traduzido = 'tabulação'
+    if delimitador == ',':
+        delimitador_traduzido = 'vírgula'
+    if delimitador == ';':
+        delimitador_traduzido = 'ponto-e-vírgula'
+    print("\n\n"+"O arquivo de entrada foi identificado como tendo codificacao " +
+          codificacao_entrada + " e delimitador "+delimitador_traduzido+"\n\n")
     #
     # A primeira linha tem os cabeçalhos (títulos dos campos)
     # Da segunda linha (registro) em diante tempos os valores dos campos
-    cabecalhos, lista = le_databook(arq_ent, delimitador)
+    cabecalhos, lista = le_databook(arq_ent, delimitador, codificacao_entrada)
 
     print("\n\n"+str(len(lista)) +
           " registros de foram encontrados no arquivo CSV e serão convertidos para uma tabela HTML com cabeçalho.")
 
     print("Se houver um arquivo 'css.css' neste diretório, ele será integrado ao arquivo 'saida.html' final;")
     print("se houver um arquivo 'antes.html' neste diretório, ele será integrado ao arquivo 'saida.html' antes da tabela;")
-    print("se houver um arquivo 'depois.html' neste diretório, ele será integrado ao arquivo 'saida.html' depois da tabela.\n\n\n")
+    print("se houver um arquivo 'depois.html' neste diretório, ele será integrado ao arquivo 'saida.html' depois da tabela.\n\n")
     x = input("Tecle <enter> para criar o arquivo 'saida.html': ")
     cria_arq_saida("saida.html", cabecalhos, lista)
     x = input("Tecle <enter> para encerrar. ")
